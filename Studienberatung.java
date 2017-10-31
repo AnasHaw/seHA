ad589d61fc51c4da17d5564ae5f28c254715f230
Anas Garwal <Anas.g@hotmail.de>
2017-10-31 16:26:23 +0100

    added class and implements question and scanner

---
+++ Studienberatung.java
@@ -0,0 +1,10 @@
+public class Studienberatung{
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(system.int);
+        String frage = "Hast du Abitur?";
+        String antwort = "";
+        
+        System.out.println(frage);
+        antwort = scanner.next();
+    }
+}
