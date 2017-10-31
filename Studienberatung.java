474e2f42e93318638b461f4543c264f832389867
Anas Garwal <Anas.g@hotmail.de>
2017-10-31 16:34:03 +0100

    initzializie and compiled

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class Studienberatung{
     public static void main (String[] args){
-        Scanner scanner = new Scanner(system.int);
+        Scanner scanner = new Scanner(System.in);
         String frage = "Hast du Abitur?";
         String antwort = "";
         
